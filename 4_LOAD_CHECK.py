31b719f737da365c59ff523c28611076dbc7c720
richardadvocaat <115482339+richardadvocaat@users.noreply.github.com>
2025-09-23 20:56:44 +0200

    Update 4_LOAD_CHECK.py
    Added a variable for the year in the Excel filename.

--- 4_LOAD_CHECK.py
+++ 4_LOAD_CHECK.py
@@ -13,8 +13,9 @@ def read_user_data(ATP_file_path, sheet_name="User_Data"):
     user_data = df.set_index('Key').to_dict()['Value']
     return user_data
 
-Athlete_TLA = "TLA" #Three letter Acronym of athlete.
-ATP_file_path = rf"C:\TEMP\{Athlete_TLA}\ATP2intervals_{Athlete_TLA}.xlsm"
+Athlete_TLA = "RAA" #Three letter Acronym of athlete.
+ATP_year = "2025" #The year of the plan.
+ATP_file_path = rf"C:\TEMP\{Athlete_TLA}\ATP2intervals_{Athlete_TLA}_{ATP_year}.xlsm"
 
 ATP_sheet_name = "ATP_Data"
 ATP_loadcheck_sheet_name = "Weekly Type Loads"
